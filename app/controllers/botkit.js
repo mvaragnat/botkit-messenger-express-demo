73501765318a6176f7e1fd423ba9048ed42bb2c0
vincent <vincent@gephen.co>
2016-08-18 10:17:06 +0800

    added some missing libraries to package.json, request import missing in botkit controller and lintage

--- app/controllers/botkit.js
+++ app/controllers/botkit.js
@@ -1,156 +1,157 @@
 //CONFIG===============================================
 
 /* Uses the slack button feature to offer a real time bot to multiple teams */
-var Botkit                = require('botkit');
-var mongoUri              = process.env.MONGODB_URI || 'mongodb://localhost/demo'
-var db                    = require('../../config/db')({mongoUri: mongoUri})
+var Botkit = require('botkit');
+var mongoUri = process.env.MONGODB_URI || 'mongodb://localhost/botkit-demo';
+var db = require('../../config/db')({mongoUri: mongoUri});
+var request = require('request');
 
 var controller = Botkit.facebookbot({
-  debug: true,
-  access_token: process.env.FACEBOOK_PAGE_TOKEN,
-  verify_token: process.env.FACEBOOK_VERIFY_TOKEN,
-  storage: db
-})
+    debug: true,
+    access_token: process.env.FACEBOOK_PAGE_TOKEN,
+    verify_token: process.env.FACEBOOK_VERIFY_TOKEN,
+    storage: db
+});
 
-var bot = controller.spawn({})
+var bot = controller.spawn({});
 
 
 //subscribe to page events
 request.post('https://graph.facebook.com/me/subscribed_apps?access_token=' + process.env.FACEBOOK_PAGE_TOKEN,
-  function(err, res, body) {
-    if (err) {
-        controller.log('Could not subscribe to page messages');
-    }
-    else {
-      controller.log('Successfully subscribed to Facebook events:', body);
-      console.log('Botkit activated')
+    function (err, res, body) {
+        if (err) {
+            controller.log('Could not subscribe to page messages');
+        }
+        else {
+            controller.log('Successfully subscribed to Facebook events:', body);
+            console.log('Botkit activated');
 
-      //start ticking to send conversation messages
-      controller.startTicking()
-    }
-})
+            //start ticking to send conversation messages
+            controller.startTicking()
+        }
+    });
 
-console.log('botkit')
+console.log('botkit');
 
 // this is triggered when a user clicks the send-to-messenger plugin
-controller.on('facebook_optin', function(bot, message) {
+controller.on('facebook_optin', function (bot, message) {
     bot.reply(message, 'Welcome, friend');
-})
+});
 
 // user said hello
-controller.hears(['hello'], 'message_received', function(bot, message) {
+controller.hears(['hello'], 'message_received', function (bot, message) {
 
     bot.reply(message, 'Hey there.');
 
-})
+});
 
 // user says anything else
-controller.hears('(.*)', 'message_received', function(bot, message) {
+controller.hears('(.*)', 'message_received', function (bot, message) {
 
     bot.reply(message, 'you said ' + message.match[1])
 
-})
-
+});
 
 
 //this function processes the POST request to the webhook
-var handler = function(obj){
-  controller.debug('GOT A MESSAGE HOOK');
-  if (obj.entry) {
-    for (var e = 0; e < obj.entry.length; e++) {
-      for (var m = 0; m < obj.entry[e].messaging.length; m++) {
-        var facebook_message = obj.entry[e].messaging[m];
+var handler = function (obj) {
+    controller.debug('GOT A MESSAGE HOOK');
+    var message;
+    if (obj.entry) {
+        for (var e = 0; e < obj.entry.length; e++) {
+            for (var m = 0; m < obj.entry[e].messaging.length; m++) {
+                var facebook_message = obj.entry[e].messaging[m];
 
-        console.log(facebook_message)
+                console.log(facebook_message);
 
-        //normal message
-        if (facebook_message.message) {
+                //normal message
+                if (facebook_message.message) {
 
-          var message = {
-              text: facebook_message.message.text,
-              user: facebook_message.sender.id,
-              channel: facebook_message.sender.id,
-              timestamp: facebook_message.timestamp,
-              seq: facebook_message.message.seq,
-              mid: facebook_message.message.mid,
-              attachments: facebook_message.message.attachments,
-          }
+                    message = {
+                        text: facebook_message.message.text,
+                        user: facebook_message.sender.id,
+                        channel: facebook_message.sender.id,
+                        timestamp: facebook_message.timestamp,
+                        seq: facebook_message.message.seq,
+                        mid: facebook_message.message.mid,
+                        attachments: facebook_message.message.attachments
+                    };
 
-          //save if user comes from m.me adress or Facebook search
-          create_user_if_new(facebook_message.sender.id, facebook_message.timestamp)
+                    //save if user comes from m.me adress or Facebook search
+                    create_user_if_new(facebook_message.sender.id, facebook_message.timestamp);
 
-          controller.receiveMessage(bot, message);
+                    controller.receiveMessage(bot, message);
+                }
+                //clicks on a postback action in an attachment
+                else if (facebook_message.postback) {
+
+                    // trigger BOTH a facebook_postback event
+                    // and a normal message received event.
+                    // this allows developers to receive postbacks as part of a conversation.
+                    message = {
+                        payload: facebook_message.postback.payload,
+                        user: facebook_message.sender.id,
+                        channel: facebook_message.sender.id,
+                        timestamp: facebook_message.timestamp
+                    };
+
+                    controller.trigger('facebook_postback', [bot, message]);
+
+                    message = {
+                        text: facebook_message.postback.payload,
+                        user: facebook_message.sender.id,
+                        channel: facebook_message.sender.id,
+                        timestamp: facebook_message.timestamp
+                    };
+
+                    controller.receiveMessage(bot, message);
+
+                }
+                //When a user clicks on "Send to Messenger"
+                else if (facebook_message.optin) {
+
+                    message = {
+                        optin: facebook_message.optin,
+                        user: facebook_message.sender.id,
+                        channel: facebook_message.sender.id,
+                        timestamp: facebook_message.timestamp
+                    };
+
+                    //save if user comes from "Send to Messenger"
+                    create_user_if_new(facebook_message.sender.id, facebook_message.timestamp);
+
+                    controller.trigger('facebook_optin', [bot, message]);
+                }
+                //message delivered callback
+                else if (facebook_message.delivery) {
+
+                    message = {
+                        optin: facebook_message.delivery,
+                        user: facebook_message.sender.id,
+                        channel: facebook_message.sender.id,
+                        timestamp: facebook_message.timestamp
+                    };
+
+                    controller.trigger('message_delivered', [bot, message]);
+
+                }
+                else {
+                    controller.log('Got an unexpected message from Facebook: ', facebook_message);
+                }
+            }
         }
-        //clicks on a postback action in an attachment
-        else if (facebook_message.postback) {
-
-          // trigger BOTH a facebook_postback event
-          // and a normal message received event.
-          // this allows developers to receive postbacks as part of a conversation.
-          var message = {
-              payload: facebook_message.postback.payload,
-              user: facebook_message.sender.id,
-              channel: facebook_message.sender.id,
-              timestamp: facebook_message.timestamp,
-          };
-
-          controller.trigger('facebook_postback', [bot, message]);
-
-          var message = {
-              text: facebook_message.postback.payload,
-              user: facebook_message.sender.id,
-              channel: facebook_message.sender.id,
-              timestamp: facebook_message.timestamp,
-          };
-
-          controller.receiveMessage(bot, message);
-
-        }
-        //When a user clicks on "Send to Messenger"
-        else if (facebook_message.optin) {
-
-          var message = {
-              optin: facebook_message.optin,
-              user: facebook_message.sender.id,
-              channel: facebook_message.sender.id,
-              timestamp: facebook_message.timestamp,
-          };
-
-          //save if user comes from "Send to Messenger"
-          create_user_if_new(facebook_message.sender.id, facebook_message.timestamp)
-
-          controller.trigger('facebook_optin', [bot, message]);
-        }
-        //message delivered callback
-        else if (facebook_message.delivery) {
-
-          var message = {
-              optin: facebook_message.delivery,
-              user: facebook_message.sender.id,
-              channel: facebook_message.sender.id,
-              timestamp: facebook_message.timestamp,
-          };
-
-          controller.trigger('message_delivered', [bot, message]);
-
-        }
-        else {
-          controller.log('Got an unexpected message from Facebook: ', facebook_message);
-        }
-      }
     }
-  }
-}
+};
 
-var create_user_if_new = function(id, ts){
-  controller.storage.users.get(id, function(err, user){
-    if(err){
-      console.log(err)
-    }
-    else if(!user){
-      controller.storage.users.save({id: id, created_at: ts})
-    }
-  })
-}
+var create_user_if_new = function (id, ts) {
+    controller.storage.users.get(id, function (err, user) {
+        if (err) {
+            console.log(err)
+        }
+        else if (!user) {
+            controller.storage.users.save({id: id, created_at: ts})
+        }
+    })
+};
 
-exports.handler = handler
+exports.handler = handler;
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,28 +1,28 @@
 var facebook_handler = require('../controllers/botkit').handler
 
-module.exports = function(app) {
-  //public pages=============================================
-  //root
-  app.get('/', function(req, res) {
-    res.render('home')
-  })
+module.exports = function (app) {
+    //public pages=============================================
+    //root
+    app.get('/', function (req, res) {
+        res.render('home')
+    });
 
-  app.get('/webhook', function(req, res) {
-    //This enables subscription to the webhooks
-    if(req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === process.env.FACEBOOK_VERIFY_TOKEN){
-      res.send(req.query['hub.challenge'])
-    }
-    else{
-      res.send("Incorrect verify token")
-    }
-  })
+    app.get('/webhook', function (req, res) {
+        //This enables subscription to the webhooks
+        if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === process.env.FACEBOOK_VERIFY_TOKEN) {
+            res.send(req.query['hub.challenge'])
+        }
+        else {
+            res.send("Incorrect verify token")
+        }
+    });
 
-  app.post('/webhook', function (req, res) {
+    app.post('/webhook', function (req, res) {
 
-    facebook_handler(req.body)
+        facebook_handler(req.body);
 
-    res.send('ok')
-  })
-}
+        res.send('ok')
+    })
+};
 
 
